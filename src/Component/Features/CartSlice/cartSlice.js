9ac57d009586813f122b72b19bf8d3281a91e729
taufik69 <taufik35-2915@diu.edu.bd>
2024-05-20 02:58:35 +0600

    add to cart funtionality done with find method

--- src/Component/Features/CartSlice/cartSlice.js
+++ src/Component/Features/CartSlice/cartSlice.js
@@ -11,33 +11,37 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      //   let itemIndex = state.cartItems.findIndex(
-      //     (item) => item.id == action.payload.id
-      //   );
-      //   if (itemIndex >= 0) {
-      //     state.cartItems[itemIndex].cartQuantity += 1;
-      //     state.cartTotalQuantity = state.cartItems[itemIndex].cartQuantity;
-      //     state.cartTotalAmount =
-      //       state.cartItems[itemIndex].cartQuantity * action.payload.price;
-      //   } else {
-      //     state.cartItems.push({ ...action.payload, cartQuantity: 1 });
-      //   }
-      //   using the help of filter method
-      let findItem = state.cartItems.filter(
-        (item) => item.id === action.payload.id
+      let itemIndex = state.cartItems.findIndex(
+        (item) => item.id == action.payload.id
       );
-
-      if (findItem.length >= 1) {
-        state.cartItems[0].cartQuantity += 1;
-        state.cartTotalQuantity = state.cartItems[0].cartQuantity;
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+        state.cartTotalQuantity = state.cartItems[itemIndex].cartQuantity;
         state.cartTotalAmount =
-          state.cartItems[0].cartQuantity * action.payload.price;
+          state.cartItems[itemIndex].cartQuantity * action.payload.price;
       } else {
         const temporary = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(temporary);
         state.cartTotalQuantity = temporary.cartQuantity;
         state.cartTotalAmount = temporary.cartQuantity * action.payload.price;
       }
+
+      //   using the help of filter method
+      //   let findItem = state.cartItems.filter(
+      //     (item) => item.id === action.payload.id
+      //   );
+
+      //   if (findItem.length >= 1) {
+      //     state.cartItems[0].cartQuantity += 1;
+      //     state.cartTotalQuantity = state.cartItems[0].cartQuantity;
+      //     state.cartTotalAmount =
+      //       state.cartItems[0].cartQuantity * action.payload.price;
+      //   } else {
+      //     const temporary = { ...action.payload, cartQuantity: 1 };
+      //     state.cartItems.push(temporary);
+      //     state.cartTotalQuantity = temporary.cartQuantity;
+      //     state.cartTotalAmount = temporary.cartQuantity * action.payload.price;
+      //   }
     },
   },
 });
