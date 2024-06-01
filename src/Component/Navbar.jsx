57f74c0e7406f116e1ec0881272fc6ec82f2e89f
taufik69 <taufik35-2915@diu.edu.bd>
2024-06-01 16:30:50 +0600

    pass value in after seudo code

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { IoCartOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 const Navbar = () => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems, cartTotalQuantity, cartTotalAmount } = cart;
+
   return (
     <nav>
       <div className="container">
         <div className="navbar">
           <div className="logo">Taufik</div>
           <div className="icon">
-            <span>
+            <span quantity={cartTotalQuantity}>
               <IoCartOutline />
             </span>
           </div>
